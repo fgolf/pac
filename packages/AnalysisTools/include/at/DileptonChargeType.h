4ff8365c794edab21f2365d98fd0f0a9289ebfd1
kelley <>
2012-08-24 04:24:46 +0000

    forgot DileptonChargeType

---
+++ packages/AnalysisTools/include/at/DileptonChargeType.h
@@ -0,0 +1,31 @@
+#ifndef DILEPTONCHARGETYPE_H
+#define DILEPTONCHARGETYPE_H
+
+#include <string>
+
+namespace at 
+{
+    // flavor
+    struct DileptonChargeType
+    {
+        enum value_type
+        {
+            SS,  // same sign
+            SF,  // single fake
+            DF,  // double fake
+            OS,  // opposite sign
+            static_size
+        };
+    };
+
+    // return the name of the name the DileptonChargeType
+    std::string GetDileptonChargeTypeName(const DileptonChargeType::value_type& charge_type);
+
+    // return the title for ROOT of the name the DileptonChargeType
+    std::string GetDileptonChargeTypeTitle(const DileptonChargeType::value_type& charge_type);
+
+    // return the title for latex of the name the DileptonChargeType
+    std::string GetDileptonChargeTypeLatex(const DileptonChargeType::value_type& charge_type);
+};
+
+#endif // DILEPTONCHARGETYPE_H 
