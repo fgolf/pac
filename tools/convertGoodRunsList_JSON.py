c612bbc799e758e2293a8657fb91384793fdd8da
kelley <>
2012-09-03 22:43:32 +0000

    add json conversion script

---
+++ tools/convertGoodRunsList_JSON.py
@@ -0,0 +1,20 @@
+#! /usr/bin/evn python
+
+# usage to print out good run selection for goodrun.cc:
+# convertGoodRunsList_JSON.py <json file>
+
+import sys
+import json
+
+runs = json.load(open(sys.argv[1],"r"))
+
+for run in runs.keys():
+    for lumiBlock in runs[run] :
+        if len(lumiBlock) != 2 :
+            print 'ERROR reading lumi block: run:',run,'lumiBlock:',lumiBlock
+        else:
+            # print 'run ' + str(run) + ', min lumi ' + str(lumiBlock[0]) + ', max lumi ' + str(lumiBlock[1])
+            print run,lumiBlock[0],lumiBlock[1]
+
+#print ''
+
