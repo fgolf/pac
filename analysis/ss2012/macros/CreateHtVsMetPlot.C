babee645613fbed908308190d57be8f2791ba4da
kelley <>
2012-10-08 23:35:59 +0000

    updated with njets differientiation

--- analysis/ss2012/macros/CreateHtVsMetPlot.C
+++ analysis/ss2012/macros/CreateHtVsMetPlot.C
@@ -15,50 +15,81 @@ void CreateHtVsMetPlot(bool btag2 = true, const std::string& suffix = "png")
     // set TDR style
     rt::SetTDRStyle();
 
-    float lumi = 10.45; // fb^-1
+    float lumi = 10.5; // fb^-1
 
     TChain e1("tree");
     e1.Add("babies/data.root");
 
-    TH2F* h2_ht_vs_pfmet_baseline_mm = new TH2F("h2_ht_vs_pfmet_baseline_mm", Form("CMS Preliminary #sqrt{s} = 8 TeV, L_{int} = %2.2f fb^{-1};H_{T} (GeV); E_{T}^{miss} (GeV)", lumi), 1000, 0, 1000, 250, 0, 250);
-    TH2F* h2_ht_vs_pfmet_baseline_em = new TH2F("h2_ht_vs_pfmet_baseline_em", Form("CMS Preliminary #sqrt{s} = 8 TeV, L_{int} = %2.2f fb^{-1};H_{T} (GeV); E_{T}^{miss} (GeV)", lumi), 1000, 0, 1000, 250, 0, 250);
-    TH2F* h2_ht_vs_pfmet_baseline_ee = new TH2F("h2_ht_vs_pfmet_baseline_ee", Form("CMS Preliminary #sqrt{s} = 8 TeV, L_{int} = %2.2f fb^{-1};H_{T} (GeV); E_{T}^{miss} (GeV)", lumi), 1000, 0, 1000, 250, 0, 250);
+    TH2F* h2_ht_vs_pfmet_baseline_njlt4_mm = new TH2F("h2_ht_vs_pfmet_baseline_njlt4_mm", Form("CMS Preliminary #sqrt{s} = 8 TeV, L_{int} = %2.1f fb^{-1};H_{T} (GeV); E_{T}^{miss} (GeV)", lumi), 1000, 0, 1000, 250, 0, 250);
+    TH2F* h2_ht_vs_pfmet_baseline_njlt4_em = new TH2F("h2_ht_vs_pfmet_baseline_njlt4_em", Form("CMS Preliminary #sqrt{s} = 8 TeV, L_{int} = %2.1f fb^{-1};H_{T} (GeV); E_{T}^{miss} (GeV)", lumi), 1000, 0, 1000, 250, 0, 250);
+    TH2F* h2_ht_vs_pfmet_baseline_njlt4_ee = new TH2F("h2_ht_vs_pfmet_baseline_njlt4_ee", Form("CMS Preliminary #sqrt{s} = 8 TeV, L_{int} = %2.1f fb^{-1};H_{T} (GeV); E_{T}^{miss} (GeV)", lumi), 1000, 0, 1000, 250, 0, 250);
+    TH2F* h2_ht_vs_pfmet_baseline_njge4_mm = new TH2F("h2_ht_vs_pfmet_baseline_njge4_mm", Form("CMS Preliminary #sqrt{s} = 8 TeV, L_{int} = %2.1f fb^{-1};H_{T} (GeV); E_{T}^{miss} (GeV)", lumi), 1000, 0, 1000, 250, 0, 250);
+    TH2F* h2_ht_vs_pfmet_baseline_njge4_em = new TH2F("h2_ht_vs_pfmet_baseline_njge4_em", Form("CMS Preliminary #sqrt{s} = 8 TeV, L_{int} = %2.1f fb^{-1};H_{T} (GeV); E_{T}^{miss} (GeV)", lumi), 1000, 0, 1000, 250, 0, 250);
+    TH2F* h2_ht_vs_pfmet_baseline_njge4_ee = new TH2F("h2_ht_vs_pfmet_baseline_njge4_ee", Form("CMS Preliminary #sqrt{s} = 8 TeV, L_{int} = %2.1f fb^{-1};H_{T} (GeV); E_{T}^{miss} (GeV)", lumi), 1000, 0, 1000, 250, 0, 250);
 
     TCut selection = btag2 ? "is_ss && njets>=2 && nbtags>=2 && is_good_lumi" : "is_ss && njets>=2 && is_good_lumi";
 
-    e1.Draw("pfmet:ht>>h2_ht_vs_pfmet_baseline_mm", selection && "dilep_type==1", "goff");
-    e1.Draw("pfmet:ht>>h2_ht_vs_pfmet_baseline_em", selection && "dilep_type==2", "goff");
-    e1.Draw("pfmet:ht>>h2_ht_vs_pfmet_baseline_ee", selection && "dilep_type==3", "goff");
+    e1.Draw("pfmet:ht>>h2_ht_vs_pfmet_baseline_njlt4_mm", selection && "dilep_type==1 && njets<4" , "goff");
+    e1.Draw("pfmet:ht>>h2_ht_vs_pfmet_baseline_njlt4_em", selection && "dilep_type==2 && njets<4" , "goff");
+    e1.Draw("pfmet:ht>>h2_ht_vs_pfmet_baseline_njlt4_ee", selection && "dilep_type==3 && njets<4" , "goff");
+    e1.Draw("pfmet:ht>>h2_ht_vs_pfmet_baseline_njge4_mm", selection && "dilep_type==1 && njets>=4", "goff");
+    e1.Draw("pfmet:ht>>h2_ht_vs_pfmet_baseline_njge4_em", selection && "dilep_type==2 && njets>=4", "goff");
+    e1.Draw("pfmet:ht>>h2_ht_vs_pfmet_baseline_njge4_ee", selection && "dilep_type==3 && njets>=4", "goff");
 
     // set style
-    h2_ht_vs_pfmet_baseline_ee->SetMarkerColor(kBlack);
-    h2_ht_vs_pfmet_baseline_em->SetMarkerColor(kRed);
-    h2_ht_vs_pfmet_baseline_mm->SetMarkerColor(kBlue);
-    h2_ht_vs_pfmet_baseline_ee->SetMarkerStyle(20);
-    h2_ht_vs_pfmet_baseline_em->SetMarkerStyle(21);
-    h2_ht_vs_pfmet_baseline_mm->SetMarkerStyle(22);
-    h2_ht_vs_pfmet_baseline_ee->SetMarkerSize(1.6);
-    h2_ht_vs_pfmet_baseline_em->SetMarkerSize(1.6);
-    h2_ht_vs_pfmet_baseline_mm->SetMarkerSize(1.6);
-    h2_ht_vs_pfmet_baseline_ee->SetStats(false);
-    h2_ht_vs_pfmet_baseline_em->SetStats(false);
-    h2_ht_vs_pfmet_baseline_mm->SetStats(false);
+    h2_ht_vs_pfmet_baseline_njlt4_ee->SetMarkerColor(kBlack);
+    h2_ht_vs_pfmet_baseline_njlt4_em->SetMarkerColor(kRed);
+    h2_ht_vs_pfmet_baseline_njlt4_mm->SetMarkerColor(kBlue);
+    h2_ht_vs_pfmet_baseline_njlt4_ee->SetMarkerStyle(24);
+    h2_ht_vs_pfmet_baseline_njlt4_em->SetMarkerStyle(25);
+    h2_ht_vs_pfmet_baseline_njlt4_mm->SetMarkerStyle(26);
+    h2_ht_vs_pfmet_baseline_njlt4_ee->SetMarkerSize(1.7);
+    h2_ht_vs_pfmet_baseline_njlt4_em->SetMarkerSize(1.7);
+    h2_ht_vs_pfmet_baseline_njlt4_mm->SetMarkerSize(1.7);
+    h2_ht_vs_pfmet_baseline_njlt4_ee->SetLineWidth(2);
+    h2_ht_vs_pfmet_baseline_njlt4_em->SetLineWidth(2);
+    h2_ht_vs_pfmet_baseline_njlt4_mm->SetLineWidth(2);
+    h2_ht_vs_pfmet_baseline_njlt4_ee->SetStats(false);
+    h2_ht_vs_pfmet_baseline_njlt4_em->SetStats(false);
+    h2_ht_vs_pfmet_baseline_njlt4_mm->SetStats(false);
+
+    h2_ht_vs_pfmet_baseline_njge4_ee->SetMarkerColor(kBlack);
+    h2_ht_vs_pfmet_baseline_njge4_em->SetMarkerColor(kRed);
+    h2_ht_vs_pfmet_baseline_njge4_mm->SetMarkerColor(kBlue);
+    h2_ht_vs_pfmet_baseline_njge4_ee->SetMarkerStyle(20);
+    h2_ht_vs_pfmet_baseline_njge4_em->SetMarkerStyle(21);
+    h2_ht_vs_pfmet_baseline_njge4_mm->SetMarkerStyle(22);
+    h2_ht_vs_pfmet_baseline_njge4_ee->SetMarkerSize(1.7);
+    h2_ht_vs_pfmet_baseline_njge4_em->SetMarkerSize(1.7);
+    h2_ht_vs_pfmet_baseline_njge4_mm->SetMarkerSize(1.7);
+    h2_ht_vs_pfmet_baseline_njge4_ee->SetLineWidth(2);
+    h2_ht_vs_pfmet_baseline_njge4_em->SetLineWidth(2);
+    h2_ht_vs_pfmet_baseline_njge4_mm->SetLineWidth(2);
+    h2_ht_vs_pfmet_baseline_njge4_ee->SetStats(false);
+    h2_ht_vs_pfmet_baseline_njge4_em->SetStats(false);
+    h2_ht_vs_pfmet_baseline_njge4_mm->SetStats(false);
 
     // draw the hists
     TCanvas* c1 = new TCanvas("c1", "c1");
-    h2_ht_vs_pfmet_baseline_ee->Draw();
-    h2_ht_vs_pfmet_baseline_em->Draw("same");
-    h2_ht_vs_pfmet_baseline_mm->Draw("same");
+    h2_ht_vs_pfmet_baseline_njlt4_ee->Draw();
+    h2_ht_vs_pfmet_baseline_njlt4_em->Draw("sames");
+    h2_ht_vs_pfmet_baseline_njlt4_mm->Draw("sames");
+    h2_ht_vs_pfmet_baseline_njge4_ee->Draw("sames");
+    h2_ht_vs_pfmet_baseline_njge4_em->Draw("sames");
+    h2_ht_vs_pfmet_baseline_njge4_mm->Draw("sames");
 
     // legend
-    TLegend* l1 = new TLegend(0.75, 0.65, 0.89, 0.84);
+    TLegend* l1 = new TLegend(0.70, 0.55, 0.89, 0.84);
     l1->SetFillColor(0);  // 0 makes it the background clear on the pad
     l1->SetFillStyle(0);
     l1->SetBorderSize(0);
     //l1->SetTextSize(0.1);
-    l1->AddEntry(h2_ht_vs_pfmet_baseline_ee, "ee"    , "p");
-    l1->AddEntry(h2_ht_vs_pfmet_baseline_em, "e#mu"  , "p");
-    l1->AddEntry(h2_ht_vs_pfmet_baseline_mm, "#mu#mu", "p");
+    l1->AddEntry(h2_ht_vs_pfmet_baseline_njlt4_ee, "ee (2 #leq # jets < 4)"       , "p");
+    l1->AddEntry(h2_ht_vs_pfmet_baseline_njlt4_em, "e#mu (2 #leq # jets < 4)"     , "p");
+    l1->AddEntry(h2_ht_vs_pfmet_baseline_njlt4_mm, "#mu#mu (2 #leq # jets < 4)"   , "p");
+    l1->AddEntry(h2_ht_vs_pfmet_baseline_njge4_ee, "ee (# jets #geq 4)"    , "p");
+    l1->AddEntry(h2_ht_vs_pfmet_baseline_njge4_em, "e#mu (# jets #geq 4)"  , "p");
+    l1->AddEntry(h2_ht_vs_pfmet_baseline_njge4_mm, "#mu#mu (# jets #geq 4)", "p");
     l1->Draw();
 
     // cut out region
@@ -76,15 +107,18 @@ void CreateHtVsMetPlot(bool btag2 = true, const std::string& suffix = "png")
     g_implied->SetFillStyle(3005);
     g_implied->Draw();
 
-    cout << "ee entried = " << h2_ht_vs_pfmet_baseline_ee->GetEntries() << endl;
-    cout << "em entried = " << h2_ht_vs_pfmet_baseline_em->GetEntries() << endl;
-    cout << "mm entried = " << h2_ht_vs_pfmet_baseline_mm->GetEntries() << endl;
-    cout << "ll entried = " << (h2_ht_vs_pfmet_baseline_ee->GetEntries() + 
-                                h2_ht_vs_pfmet_baseline_em->GetEntries() +
-                                h2_ht_vs_pfmet_baseline_mm->GetEntries()) << endl;
+    cout << "ee nj2 entried = " << h2_ht_vs_pfmet_baseline_njlt4_ee->GetEntries() << endl;
+    cout << "em nj2 entried = " << h2_ht_vs_pfmet_baseline_njlt4_em->GetEntries() << endl;
+    cout << "mm nj2 entried = " << h2_ht_vs_pfmet_baseline_njlt4_mm->GetEntries() << endl;
+    cout << "ee nj4 entried = " << h2_ht_vs_pfmet_baseline_njge4_ee->GetEntries() << endl;
+    cout << "em nj4 entried = " << h2_ht_vs_pfmet_baseline_njge4_em->GetEntries() << endl;
+    cout << "mm nj4 entried = " << h2_ht_vs_pfmet_baseline_njge4_mm->GetEntries() << endl;
+    cout << "ll entried = " << (h2_ht_vs_pfmet_baseline_njlt4_ee->GetEntries() + h2_ht_vs_pfmet_baseline_njge4_ee->GetEntries() +
+                                h2_ht_vs_pfmet_baseline_njlt4_em->GetEntries() + h2_ht_vs_pfmet_baseline_njge4_em->GetEntries() +
+                                h2_ht_vs_pfmet_baseline_njlt4_mm->GetEntries() + h2_ht_vs_pfmet_baseline_njge4_mm->GetEntries()) << endl;
 
     if (!suffix.empty())
     {
-        c1->Print(Form("plots/note/HtVsMET_%s.%s", (btag2 ? "baseline" : "ht30met30"), suffix.c_str()));
+        c1->Print(Form("plots/note/HtVsMET_%s.%s", (btag2 ? "baseline" : "ht80met30"), suffix.c_str()));
     }
 }
