8b073aba5d5d016b2f8531ef02662c0907ba85e0
fgolf <>
2012-10-30 16:56:37 +0000

    linkdef file

---
+++ analysis/ttv2012/source/LinkDef.h
@@ -0,0 +1,17 @@
+//------------------------------------------------------------------------//
+// SS2012 Tools LinkDef.h Kelley 2012-09-07
+//------------------------------------------------------------------------//
+
+#ifdef __CINT__  // interpreter and ACLiC
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions; 
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+// namespaces
+//#pragma link C++ namespace ss; 
+
+#endif // __CINT__
