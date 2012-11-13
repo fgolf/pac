b40eb2566157419179d5c548dddd5e9510ff5749
fgolf <>
2012-11-13 19:08:51 +0000

    clean up

--- analysis/ss2012/include/FakeRateBabyLooper.h
+++ analysis/ss2012/include/FakeRateBabyLooper.h
@@ -6,38 +6,37 @@
 
 class FakeRateBabyLooper : public at::AnalysisWithHist
 {
-    public:
-        // construct:
-        FakeRateBabyLooper
-        (
-             const std::string& root_file_name,
-             const std::string& dataset,
-             const std::string& lepton,
-			 float lumi = 1.0,
-             int charge = 0,
-             bool verbose = false,
-             bool print = false,
-             const std::string& suffix = "png"
+public:
+    // construct:
+    FakeRateBabyLooper
+    (
+        const std::string& root_file_name,
+        const std::string& dataset,
+        const std::string& lepton,
+        float lumi = 1.0,
+        int charge = 0,
+        bool verbose = false,
+        bool print = false,
+        const std::string& suffix = "png"
         );
 
-        // destroy:
-        ~FakeRateBabyLooper();
+    // destroy:
+    ~FakeRateBabyLooper();
 
-        // function operator:
-        int operator()(long event);
+    // function operator:
+    int operator()(long event);
 
-        // members
-        virtual void BeginJob();
-        virtual void EndJob();
-        virtual void BookHists();
+    // members
+    virtual void BeginJob();
+    virtual void EndJob();
+    virtual void BookHists();    
 
-    private:
-        // members
-        std::string m_dataset;
-        std::string m_lepton;
-		float m_lumi;
-        int m_charge;
-        bool m_verbose;
+private:
+    // members
+    std::string m_dataset;
+    std::string m_lepton;
+    float m_lumi;
+    int m_charge;
 };
 
 #endif // SS_FAKERATEBABYLOOPER_HPP
