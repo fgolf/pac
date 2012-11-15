640026cd6b4fac33e4bf6053564ffe69217bfe56
kelley <>
2012-11-15 18:57:20 +0000

    updated to use create_fr.sh to use *.exe

--- analysis/ss2012/scripts/create_fr.sh
+++ analysis/ss2012/scripts/create_fr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 label=12p26fb
-date=08Oct2012
+tag=15Nov2012
 run_list=json/preapproval_v3_cms2.txt
 
-#ss2012_create_fakerate --nev -1 --dataset data --channel el --root_file_name electrons_${label}.root --run_list $run_list
-#ss2012_create_fakerate --nev -1 --dataset data --channel mu --root_file_name muons_${label}.root     --run_list $run_list
-hadd -f data/fake_rates/ssFR_data_standard_${date}.root plots/fake_rates/muons_${label}/muons_${label}.root plots/fake_rates/electrons_${label}/electrons_${label}.root 
+ss2012_create_fakerate.exe --nev -1 --dataset data --channel el --root_file_name electrons_${tag}.root --run_list $run_list
+ss2012_create_fakerate.exe --nev -1 --dataset data --channel mu --root_file_name muons_${tag}.root     --run_list $run_list
+hadd -f data/fake_rates/ssFR_data_standard_${date}.root plots/fake_rates/muons_${tag}/muons_${tag}.root plots/fake_rates/electrons_${tag}/electrons_${tag}.root 
