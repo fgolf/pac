b56ea17833f692160a5b26c7b2ef298a791ecb7b
fgolf <>
2013-02-11 16:57:19 +0000

    add case to run high_pt_eth analysis

--- analysis/ss2012/scripts/processDataCMS2.C
+++ analysis/ss2012/scripts/processDataCMS2.C
@@ -13,39 +13,40 @@ void processData(std::string fileid, int datatype, std::string filename, int rea
     const char* flip_rate_file = "fliprate42X.root";
 
     // command
-	std::string command = "./ss2012_analysis ";
-	command.append(Form(" --input %s"       , filename.c_str())); 
-	command.append(Form(" --output %s.root" , fileid.c_str()  )); 
-	command.append(Form(" --fr %s"          , fake_rate_file  )); 
-	command.append(Form(" --fl %s"          , flip_rate_file  )); 
-	command.append(" --njets 2 "); 
-	//command.append(" --nev 1000 "); 
+    std::string command = "./ss2012_analysis ";
+    command.append(Form(" --input %s"       , filename.c_str())); 
+    command.append(Form(" --output %s.root" , fileid.c_str()  )); 
+    command.append(Form(" --fr %s"          , fake_rate_file  )); 
+    command.append(Form(" --fl %s"          , flip_rate_file  )); 
+    command.append(" --njets 2 "); 
+    //command.append(" --nev 1000 "); 
 
-	//if (!runlist.empty())
-	//{
-	//	command.append(Form(" --run_list %s", runlist.c_str()));
-	//}
-	if (!runlist.empty())
-	{
-		command.append(Form(" --sample %s", runlist.c_str()));
-	}
+    //if (!runlist.empty())
+    //{
+    //	command.append(Form(" --run_list %s", runlist.c_str()));
+    //}
+    if (!runlist.empty())
+    {
+        command.append(Form(" --sample %s", runlist.c_str()));
+    }
     switch(datatype)
     {
-        case 1: command.append(" --isFastSim 1"); break;
-        case 2: command.append(" --anal_type low_pt"); break;
-        case 3: command.append(" --anal_type low_pt --isFastSim 1"); break;
-        default: break;
+    case 1: command.append(" --isFastSim 1"); break;
+    case 2: command.append(" --anal_type low_pt"); break;
+    case 3: command.append(" --anal_type low_pt --isFastSim 1"); break;
+    case 5: command.append(" --anal_type high_pt_eth"); break;
+    default: break;
     };
     switch(realdata)
     {
-		case 1: 
-            command.append(" --sparms 1"); 
-            break;
-        default: break;
-	}
-	cout << command << endl;
+    case 1: 
+        command.append(" --sparms 1"); 
+        break;
+    default: break;
+    }
+    cout << command << endl;
 
     // execute
     gSystem->AddDynamicPath(gSystem->Getenv("$PWD"));
-	gSystem->Exec(command.c_str());
+    gSystem->Exec(command.c_str());
 }
