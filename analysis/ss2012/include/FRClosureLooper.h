85312db8382a5edad0b62dda4bf7a0758cd239d2
fgolf <>
2013-02-14 05:13:37 +0000

    modify to handle different fake rates, binning

--- analysis/ss2012/include/FRClosureLooper.h
+++ analysis/ss2012/include/FRClosureLooper.h
@@ -10,64 +10,67 @@
 #include <tr1/memory>
 #include <vector>
 #include "TH2F.h"
+#include "FakeRateType.h"
 
 class FRClosureLooper : public at::AnalysisWithHist
 {
-    public:
-        // construct:
-        FRClosureLooper
-        (
-             const std::string& root_file_name,
-             at::Sample::value_type sample,
-        	 ss::SignalRegion::value_type signal_region = ss::SignalRegion::sr0,
-        	 ss::AnalysisType::value_type analysis_type = ss::AnalysisType::high_pt,
-        	 ss::SignalRegionType::value_type signal_region_type = ss::SignalRegionType::inclusive,
-             const std::string& vtxreweight_file_name = "",
-             const std::string& fake_rate_file_name = "",
-             const std::string& mufr_hist_name = "",
-             const std::string& elfr_hist_name = "",
-             bool do_scale_factors = true,
-             unsigned int num_btags = 0,
-             unsigned int num_jets = 0,
-             int charge_option = 0,
-             float lumi = 1.0,
-             bool verbose = false
+public:
+    // construct:
+    FRClosureLooper
+    (
+        const std::string& root_file_name,
+        at::Sample::value_type sample,
+        ss::SignalRegion::value_type signal_region = ss::SignalRegion::sr0,
+        ss::AnalysisType::value_type analysis_type = ss::AnalysisType::high_pt,
+        ss::SignalRegionType::value_type signal_region_type = ss::SignalRegionType::inclusive,
+        const std::string& vtxreweight_file_name = "",
+        const std::string& fake_rate_file_name = "",
+        const std::string& mufr_hist_name = "",
+        const std::string& elfr_hist_name = "",
+        bool do_scale_factors = true,
+        unsigned int num_btags = 0,
+        unsigned int num_jets = 0,
+        int charge_option = 0,
+        float lumi = 1.0,
+        bool verbose = false
         );
 
-        // destroy:
-        ~FRClosureLooper();
+    // destroy:
+    ~FRClosureLooper();
 
-        // function operator:
-        int operator()(long event);
+    // function operator:
+    int operator()(long event);
 
-        // members
-        virtual void BeginJob();
-        virtual void EndJob();
-        virtual void BookHists();
+    // members
+    virtual void BeginJob();
+    virtual void EndJob();
+    virtual void BookHists();
 
-    private:
-        // members
-        float m_lumi;
-        bool m_verbose;
-        bool m_is_data;
-        bool m_do_vtx_reweight;
-        bool m_do_scale_factors;
-        unsigned int m_nbtags;
-        unsigned int m_njets;
-        int m_charge_option;
-        at::Sample::value_type m_sample;
-        ss::SignalRegion::value_type m_signal_region;
-        ss::AnalysisType::value_type m_analysis_type;
-        ss::SignalRegionType::value_type m_signal_region_type;
-        float m_scale1fb;
+private:
+    // members
+    float m_lumi;
+    bool m_verbose;
+    bool m_is_data;
+    bool m_do_vtx_reweight;
+    bool m_do_scale_factors;
+    unsigned int m_nbtags;
+    unsigned int m_njets;
+    int m_charge_option;
+    at::Sample::value_type m_sample;
+    ss::SignalRegion::value_type m_signal_region;
+    ss::AnalysisType::value_type m_analysis_type;
+    ss::SignalRegionType::value_type m_signal_region_type;
+    float m_scale1fb;
+    ss::FakeRateBinInfo m_fr_bin_info;
 
-        // fake/flip rate hists
-        std::tr1::shared_ptr<TH2F> h_mufr;
-        std::tr1::shared_ptr<TH2F> h_elfr;
+    // fake/flip rate hists
+    std::tr1::shared_ptr<TH2F> h_mufr;
+    std::tr1::shared_ptr<TH2F> h_elfr;
 
-        // methods
-        float GetFakeRateValue(int lep_id, float pt, float eta) const;
-        float GetFakeRateError(int lep_id, float pt, float eta) const;
+    // methods
+    float GetFakeRateValue(int lep_id, float pt, float eta) const;
+    float GetFakeRateError(int lep_id, float pt, float eta) const;
+    ss::FakeRateBinInfo GetFakeRateBinInfo();
 };
 
 #endif // FRCLOSURELOOPER_HPP
