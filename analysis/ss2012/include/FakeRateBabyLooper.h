b477d9b1039240d874e3394db2139c59130da2b7
fgolf <>
2013-02-14 05:14:25 +0000

    support different fake rate binning; fake rate definitions

--- analysis/ss2012/include/FakeRateBabyLooper.h
+++ analysis/ss2012/include/FakeRateBabyLooper.h
@@ -1,6 +1,7 @@
 #ifndef SS_FAKERATEBABYLOOPER_H
 #define SS_FAKERATEBABYLOOPER_H
 
+#include "FakeRateType.h"
 #include "at/AnalysisWithHist.h"
 #include "fr/Sample.h"
 #include <string>
@@ -14,12 +15,15 @@ public:
         const std::string& root_file_name,
         fr::Sample::value_type sample,
         const std::string& lepton,
+        const ss::FakeRateType::value_type& fr_type,
+        bool apply_tight_d0_cut = true,
+        bool use_eth_binning = false,
         float lumi = 1.0,
         int charge = 0,
         bool verbose = false,
         bool print = false,
         const std::string& suffix = "png"
-    );
+        );
 
     // destroy:
     ~FakeRateBabyLooper();
@@ -36,9 +40,13 @@ private:
     // members
     fr::Sample::value_type m_sample;
     std::string m_lepton;
-    float m_lumi;
+    ss::FakeRateType::value_type m_fr_type;
+    bool m_apply_tight_d0_cut;
     int m_charge;
+    float m_lumi;
     bool m_verbose;
+    bool m_use_eth_binning;
+    ss::FakeRateBinInfo m_fr_bin_info;
 };
 
 #endif // SS_FAKERATEBABYLOOPER_HPP
