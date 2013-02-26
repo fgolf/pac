2fa061aa4de42638b9227e81994cb259282d392c
kelley <>
2013-02-26 17:08:22 +0000

    updated to use latest CORE tag

--- tools/setup_pac.sh
+++ tools/setup_pac.sh
@@ -19,7 +19,7 @@ echo
 # --------------------------------------------------------------- #
 
 echo Checking out cms2
-tag=ss2012_V02-04-01
+tag=ss2012_V02-05-03
 #tag=HEAD
 pushd externals/build/cms2_core
 ./checkout.sh $tag
