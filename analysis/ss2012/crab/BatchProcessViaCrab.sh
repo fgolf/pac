bc67ad95b7ef053ba18b3b5af5f83fa2b2d02ba7
kelley <>
2013-02-27 03:17:51 +0000

    fix to BatchProcessViaCrab.sh to put copy the input.tgz to local dir

--- analysis/ss2012/crab/BatchProcessViaCrab.sh
+++ analysis/ss2012/crab/BatchProcessViaCrab.sh
@@ -5,7 +5,6 @@
 # to submit a looper job
 #
 
-
 if [ ! $# -eq 6 ]; then
     echo "
 USAGE: ./BatchProcessViaCrab.sh PSET TASK ARGS
@@ -29,6 +28,9 @@ CRABCFG=crab_${TASK}.cfg
 WRAPPER=wrapper_${TASK}.sh
 INPUTS=input.tgz
 WORKING_DIR="/nfs-7/userdata/${USER}/babies/cms2_V05-03-23_ss2012_V02-05-01"
+
+# move the inputs.tgz to local dir
+mv job_inputs/${INPUTS} .
 
 JOBFLAGS="events_per_job          = 10000
 total_number_of_events  = -1"
