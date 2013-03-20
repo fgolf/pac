633f72d682fbea2d9b15765b5ddd4c126b23e172
kelley <>
2013-03-20 17:38:29 +0000

    updated scale factors for lepton id/iso

--- analysis/ss2012/source/ScaleFactors.cc
+++ analysis/ss2012/source/ScaleFactors.cc
@@ -21,7 +21,98 @@ float DileptonTagAndProbeScaleFactor(const int l1_id, const LorentzVector& l1_p4
 
 float TagAndProbeScaleFactor(int id, float pt, float eta)
 {
-    return 1.0; // decided to take one
+    if (abs(id)==11) 
+    {
+        const double aeta = fabs(eta);
+
+        if (10 < pt && pt < 15)
+        {
+            if (0.00   < aeta && aeta < 0.80  ) {return 0.834;}
+            if (0.80   < aeta && aeta < 1.4442) {return 0.973;}
+            if (1.566  < aeta && aeta < 2.00  ) {return 0.954;}
+            if (2.00   < aeta && aeta < 2.50  ) {return 1.119;}
+        }
+        else if (15 < pt && pt < 20)
+        {
+            if (0.00   < aeta && aeta < 0.80  ) {return 0.918;}
+            if (0.80   < aeta && aeta < 1.4442) {return 0.906;}
+            if (1.566  < aeta && aeta < 2.00  ) {return 0.909;}
+            if (2.00   < aeta && aeta < 2.50  ) {return 0.944;}
+        }
+        else if (20 < pt && pt < 30)
+        {
+            if (0.00   < aeta && aeta < 0.80  ) {return 0.954;}
+            if (0.80   < aeta && aeta < 1.4442) {return 0.923;}
+            if (1.566  < aeta && aeta < 2.00  ) {return 0.921;}
+            if (2.00   < aeta && aeta < 2.50  ) {return 0.993;}
+        }
+        else if (30 < pt && pt < 40)
+        {
+            if (0.00   < aeta && aeta < 0.80  ) {return 0.960;}
+            if (0.80   < aeta && aeta < 1.4442) {return 0.935;}
+            if (1.566  < aeta && aeta < 2.00  ) {return 0.924;}
+            if (2.00   < aeta && aeta < 2.50  ) {return 0.959;}
+        }
+        else if (40 < pt && pt < 50)
+        {
+            if (0.00   < aeta && aeta < 0.80  ) {return 0.972;}
+            if (0.80   < aeta && aeta < 1.4442) {return 0.955;}
+            if (1.566  < aeta && aeta < 2.00  ) {return 0.950;}
+            if (2.00   < aeta && aeta < 2.50  ) {return 0.968;}
+        }
+        else if (pt > 50)
+        {
+            if (0.00   < aeta && aeta < 0.80  ) {return 0.969;}
+            if (0.80   < aeta && aeta < 1.4442) {return 0.956;}
+            if (1.566  < aeta && aeta < 2.00  ) {return 0.995;}
+            if (2.00   < aeta && aeta < 2.50  ) {return 0.969;}
+        }
+
+        // if we get here, return bogus value
+        return -999999.0;
+    }
+
+    if (abs(id)==13) 
+    {
+        const double aeta = fabs(eta);
+
+        if (10 < pt && pt < 15)
+        {
+            if (0.00  < aeta && aeta < 1.20) {return 0.973;}
+            if (1.20  < aeta && aeta < 2.50) {return 0.954;}
+        }
+        else if (15 < pt && pt < 20)
+        {
+            if (0.00  < aeta && aeta < 1.20) {return 0.957;}
+            if (1.20  < aeta && aeta < 2.50) {return 0.971;}
+        }
+        else if (20 < pt && pt < 30)
+        {
+            if (0.00  < aeta && aeta < 1.20) {return 0.964;}
+            if (1.20  < aeta && aeta < 2.50) {return 0.981;}
+        }
+        else if (30 < pt && pt < 40)
+        {
+            if (0.00  < aeta && aeta < 1.20) {return 0.971;}
+            if (1.20  < aeta && aeta < 2.50) {return 0.978;}
+        }
+        else if (40 < pt && pt < 50)
+        {
+            if (0.00  < aeta && aeta < 1.20) {return 0.978;}
+            if (1.20  < aeta && aeta < 2.50) {return 0.984;}
+        }
+        else if (pt > 50)
+        {
+            if (0.00  < aeta && aeta < 1.20) {return 0.974;}
+            if (1.20  < aeta && aeta < 2.50) {return 0.977;}
+        }
+
+        // if we get here, return bogus value
+        return -999999.0;
+    }
+
+    // if we get here, return bogus value
+    return -999999.0;
 }
 
 float DileptonTriggerScaleFactor
