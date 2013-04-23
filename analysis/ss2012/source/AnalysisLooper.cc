8e267b2a7b7fa27c8227aab80d75dbbf91416696
kelley <>
2013-04-23 22:44:16 +0000

    fixed typo in fake rate vlow pt option

--- analysis/ss2012/source/AnalysisLooper.cc
+++ analysis/ss2012/source/AnalysisLooper.cc
@@ -583,7 +583,7 @@ SSAnalysisLooper::SSAnalysisLooper
             elfr_name = "h_elfr40c_noiso_ewkcor";
             break;
         case AnalysisType::vlow_pt:
-            mufr_name = "h_mufr40c_iso_ewkcore";
+            mufr_name = "h_mufr40c_iso_ewkcor";
             elfr_name = "h_elfr40c_noiso_ewkcor";
             break;
         case AnalysisType::high_pt_eth:
