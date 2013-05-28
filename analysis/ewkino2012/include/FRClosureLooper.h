6292a656237abc17ed2ed9bc7e14da5b73a346b1
fgolf <>
2013-05-28 07:16:47 +0000

    run ttbar fake rate closure tests

---
+++ analysis/ewkino2012/include/FRClosureLooper.h
@@ -0,0 +1,82 @@
+#ifndef FRCLOSURELOOPER_HPP
+#define FRCLOSURELOOPER_HPP
+
+#include "at/AnalysisWithHist.h"
+#include "at/Sample.h"
+#include "at/DileptonHypType.h"
+#include "SignalRegion.h"
+#include <string>
+#include <tr1/array>
+#include <tr1/memory>
+#include <vector>
+#include "TH2F.h"
+#include "FakeRateType.h"
+
+class FRClosureLooper : public at::AnalysisWithHist
+{
+public:
+    // construct:
+    FRClosureLooper
+    (
+        const std::string& root_file_name,
+        const at::Sample::value_type sample,
+        const ewkino::SignalRegion::value_type signal_region = ewkino::SignalRegion::sr0,
+        const ewkino::AnalysisType::value_type analysis_type = ewkino::AnalysisType::ss,
+        const ewkino::SignalRegionType::value_type signal_region_type = ewkino::SignalRegionType::inclusive,
+        const std::string& vtxreweight_file_name = "",
+        const std::string& fake_rate_file_name = "",
+        const std::string& mufr_hist_name = "",
+        const std::string& elfr_hist_name = "",
+        const bool do_scale_factors = true,
+        const bool do_scale1fb = false,
+        const unsigned int num_btags = 0,
+        const unsigned int num_jets = 2,
+        const float met_cut = -99999,
+        const float ht_cut = -99999,
+        const int charge_option = 0,
+        const float lumi = 1.0,
+        const bool verbose = false
+    );
+
+    // destroy:
+    ~FRClosureLooper();
+
+    // function operator:
+    int operator()(long event);
+
+    // members
+    virtual void BeginJob();
+    virtual void EndJob();
+    virtual void BookHists();
+
+private:
+    // members
+    float m_lumi;
+    bool m_verbose;
+    bool m_is_data;
+    bool m_do_vtx_reweight;
+    bool m_do_scale_factors;
+    bool m_do_scale1fb;
+    unsigned int m_nbtags;
+    unsigned int m_njets;
+    float m_met_cut;
+    float m_ht_cut;
+    int m_charge_option;
+    at::Sample::value_type m_sample;
+    ewkino::SignalRegion::value_type m_signal_region;
+    ewkino::AnalysisType::value_type m_analysis_type;
+    ewkino::SignalRegionType::value_type m_signal_region_type;
+    float m_scale1fb;
+    at::FakeRateBinInfo m_fr_bin_info;
+
+    // fake/flip rate hists
+    std::tr1::shared_ptr<TH2F> h_mufr;
+    std::tr1::shared_ptr<TH2F> h_elfr;
+
+    // methods
+    float GetFakeRateValue(int lep_id, float pt, float eta) const;
+    float GetFakeRateError(int lep_id, float pt, float eta) const;
+    at::FakeRateBinInfo GetFakeRateBinInfo();
+};
+
+#endif // FRCLOSURELOOPER_HPP
---
+++ analysis/ewkino2012/source/ewkino2012_ss_closure_test.cc
@@ -0,0 +1,207 @@
+#include <iostream>
+#include <string>
+#include "TChain.h"
+#include "FRClosureLooper.h"
+#include "EWKINO2012Wrapper_SS.h"
+#include "at/ScanChain.h"
+#include "rt/RootTools.h"
+#include "SignalRegion.h"
+#include <boost/program_options.hpp>
+
+int main(int argc, char* argv[])
+try
+{
+    // set the style
+    rt::SetStyle("emruoi");
+
+    using namespace std;
+    using namespace ewkino;
+
+    // inputs
+    // -------------------------------------------------------------------------------------------------//
+
+    long number_of_events           = -1;
+    std::string input_file          = "";
+    std::string output_file         = "";
+    std::string fake_rate_file_name = "data/fake_rates/ssFR_data_standard_26Feb2013.root";
+    std::string mufr_hist_name      = "h_mufr40c";
+    std::string elfr_hist_name      = "h_elfr40c";
+    std::string vtxreweight_file    = "";
+    std::string sample_name         = "";
+    std::string analysis_type_name  = "ss";
+    bool exclusive                  = false;
+    bool do_scale_factors           = false;
+    bool do_scale1fb                = false;
+    unsigned int num_btags          = 0;
+    unsigned int num_jets           = 2;
+    float met_cut                   = 0;
+    float ht_cut                    = 0;
+    int charge_option               = 0;
+    unsigned int signal_region_num  = 0;
+    float lumi                      = 1.0;
+    bool verbose                    = false;
+
+    namespace po = boost::program_options;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help"     , "print this menu")
+        ("nev"      , po::value<long>(&number_of_events)          , "number of events to run on (-1 == all)"                    )
+        ("output"   , po::value<std::string>(&output_file)        , "name of output root file"                                  )
+        ("input"    , po::value<std::string>(&input_file)         , "name of input root file"                                   )
+        ("sample"   , po::value<std::string>(&sample_name)        , "name of input sample (from at/Sample.h)"                   )
+        ("fr_file"  , po::value<std::string>(&fake_rate_file_name), "fake rate file name"                                       )
+        ("anal_type", po::value<std::string>(&analysis_type_name) , "name of analysis type (from AnalysisType.h)"               )
+        ("mufr"     , po::value<std::string>(&mufr_hist_name)     , "muon fake rate hist name"                                  )
+        ("elfr"     , po::value<std::string>(&elfr_hist_name)     , "electron fake rate hist name"                              )
+        ("vtx_file" , po::value<std::string>(&vtxreweight_file)   , "ROOT file for the vertex reweight (ignored for data)"      )
+        ("nbtags"   , po::value<unsigned int>(&num_btags)         , "number of btags to cut on"                                 )
+        ("njets"    , po::value<unsigned int>(&num_jets)          , "number of jets to cut on"                                  )
+        ("met"      , po::value<float>(&met_cut)                  , "met to cut on"                                             )
+        ("ht"       , po::value<float>(&ht_cut)                   , "ht to cut on"                                              )
+        ("sr"       , po::value<unsigned int>(&signal_region_num) , "signal region number"                                      )
+        //("do_sf"    , po::value<bool>(&do_scale_factors)          , "use the scale factors (default is true)"                   )
+        ("do_1fb"   , po::value<bool>(&do_scale1fb)               , "use the scale1fb (default is false)"                       )
+        ("lumi"     , po::value<float>(&lumi)                     , "luminosity"                                                )
+        ("charge"   , po::value<int>(&charge_option)              , "charge option (1 is ++ events, -1 is -- events, 0 is both)")
+        ("excl"     , po::value<bool>(&exclusive)                 , "use exclusive signal region"                               )
+        ("verbose"  , po::value<bool>(&verbose)                   , "verbosity"                                                 )
+        ;
+
+    po::variables_map vm;
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    }
+    catch(const std::exception& e)
+    {
+        cerr << e.what() << "\nexiting" << endl;
+        return 1;
+    }
+
+    if (vm.count("help")) {
+        cout << desc << "\n";
+        return 1;
+    }
+
+    // check that input file exists and is specified
+    //if (!input_file.empty())
+    //{ 
+    //    if (!rt::exists(input_file))
+    //    {
+    //        cout << "ERROR: ss2012_plots: input file " << input_file << " not found" << endl;
+    //        cout << desc << "\n";
+    //        return 1;
+    //    }
+    //}
+
+    // do the main analysis
+    // -------------------------------------------------------------------------------------------------//
+
+    // analysis type
+    const ewkino::AnalysisType::value_type analysis_type = ewkino::GetAnalysisTypeFromName(analysis_type_name); 
+    const ewkino::AnalysisTypeInfo ati                   = ewkino::GetAnalysisTypeInfo(analysis_type); 
+
+    // signal region and type
+    const SignalRegionType::value_type signal_region_type = (exclusive ? SignalRegionType::exclusive : SignalRegionType::inclusive);
+    const std::string signal_region_type_name             = GetSignalRegionTypeName(signal_region_type);
+    const string signal_region_name                       = Form("sr%d", signal_region_num);
+    const SignalRegion::value_type signal_region          = GetSignalRegionFromName(signal_region_name, ati.name, signal_region_type_name);
+    const SignalRegionInfo signal_region_info             = GetSignalRegionInfo(signal_region, analysis_type, signal_region_type);
+
+    // input
+    TChain* chain  = NULL;
+    bool is_data   = false;
+    bool is_signal = false;
+    at::Sample::value_type sample = at::Sample::static_size;
+    if (sample_name.empty())  // use input file
+    {
+        if (input_file.empty())
+        {
+            cout << "ERROR: ss2012_plots: input file or sample not found -- specify one or the other" << endl; 
+            cout << desc << "\n";
+            return 1;
+        }
+        else
+        {
+            //chain = new TChain("tree");
+            //chain->Add(input_file.c_str());
+            chain = rt::CreateTChainFromCommaSeperatedList(input_file, "tree");
+            if (output_file.empty())
+            {
+                output_file = "plots/test/test.root";
+            }
+        }
+    }
+    else // use the sample
+    {
+        cout << "processing "  << sample_name << endl;
+        sample    = at::GetSampleFromName(sample_name);
+        is_data   = (at::GetSampleInfo(sample).type == at::SampleType::data);
+        is_signal = false; // not used 
+        if (input_file.empty())
+        {
+            input_file = Form("babies/%s/%s.root", ati.short_name.c_str(), sample_name.c_str());
+            //                 // special case for ttbar breakdown
+            //                 switch (sample)
+            //                 {
+            //                     case at::Sample::ttdil: input_file = Form("babies/%s/ttdil.root", ati.short_name.c_str()); break; 
+            //                     case at::Sample::ttotr: input_file = Form("babies/%s/ttotr.root", ati.short_name.c_str()); break;
+            //                     case at::Sample::ttslb: input_file = Form("babies/%s/ttslb.root", ati.short_name.c_str()); break;
+            //                     case at::Sample::ttslo: input_file = Form("babies/%s/ttslo.root", ati.short_name.c_str()); break;
+            //                     default: {/*do nothing*/}
+            //                 }
+            chain = new TChain("tree");
+            chain->Add(input_file.c_str());
+        }
+        else
+        {
+            chain = rt::CreateTChainFromCommaSeperatedList(input_file, "tree");
+        }
+        if (output_file.empty())
+        {
+            output_file = Form("plots/closure_test/%s/%s.root", signal_region_info.name.c_str(), sample_name.c_str());
+        }
+        cout << "input file(s) = " << input_file << endl;
+    }
+
+    // scan the chain
+    ScanChain
+    (
+       chain,
+       FRClosureLooper
+       (
+            output_file,       
+            sample,
+            signal_region,
+            analysis_type,
+            signal_region_type,
+            vtxreweight_file,
+            fake_rate_file_name,
+            mufr_hist_name,
+            elfr_hist_name,
+            do_scale_factors,
+            do_scale1fb,
+            num_btags,
+            num_jets,
+            met_cut,
+            ht_cut,
+            charge_option,
+            lumi,
+            verbose
+        ),
+        ewk12ss,
+        number_of_events,
+        /*good_run_file=*/"",
+        is_data,
+        verbose
+    );
+
+    // done 
+    return 0;
+}
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
