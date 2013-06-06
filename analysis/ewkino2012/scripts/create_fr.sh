9a246c3c817dd2658d03bb87554492ecaf4d9eb2
fgolf <>
2013-06-06 15:25:49 +0000

    create data, MC fake rates

---
+++ analysis/ewkino2012/scripts/create_fr.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+
+version=v14May2013
+nev=-1
+
+# create output dir
+mkdir -p plots/fake_rates/${version}
+
+# run data
+run_list=json/final_19p49fb_cms2.txt
+ewkino2012_ss_create_fakerate --nev $nev --sample data --channel el --root_file_name electrons_data_${version}.root --run_list $run_list
+ewkino2012_ss_create_fakerate --nev $nev --sample data --channel mu --root_file_name muons_data_${version}.root     --run_list $run_list
+hadd -f plots/fake_rates/${version}/ssFR_data_standard_${version}.root plots/fake_rates/muons_data_${version}/muons_data_${version}.root plots/fake_rates/electrons_data_${version}/electrons_data_${version}.root 
+
+# run DY
+run_list=\"\"
+ewkino2012_ss_create_fakerate --nev $nev --sample dy --channel el --root_file_name electrons_dy_${version}.root --run_list $run_list
+ewkino2012_ss_create_fakerate --nev $nev --sample dy --channel mu --root_file_name muons_dy_${version}.root     --run_list $run_list
+hadd -f plots/fake_rates/${version}/ssFR_dy_standard_${version}.root plots/fake_rates/muons_dy_${version}/muons_dy_${version}.root plots/fake_rates/electrons_dy_${version}/electrons_dy_${version}.root 
+
+# run wjets
+run_list=\"\"
+ewkino2012_ss_create_fakerate --nev $nev --sample wjets --channel el --root_file_name electrons_wjets_${version}.root --run_list $run_list
+ewkino2012_ss_create_fakerate --nev $nev --sample wjets --channel mu --root_file_name muons_wjets_${version}.root     --run_list $run_list
+hadd -f plots/fake_rates/${version}/ssFR_wjets_standard_${version}.root plots/fake_rates/muons_wjets_${version}/muons_wjets_${version}.root plots/fake_rates/electrons_wjets_${version}/electrons_wjets_${version}.root 
+
+# run qcd
+run_list=\"\"
+ewkino2012_ss_create_fakerate --nev $nev --sample qcd --channel el --root_file_name electrons_qcd_${version}.root --run_list $run_list
+ewkino2012_ss_create_fakerate --nev $nev --sample qcd --channel mu --root_file_name muons_qcd_${version}.root     --run_list $run_list
+hadd -f plots/fake_rates/${version}/ssFR_qcd_standard_${version}.root plots/fake_rates/muons_qcd_${version}/muons_qcd_${version}.root plots/fake_rates/electrons_qcd_${version}/electrons_qcd_${version}.root 
+
+# combine the output
+cmd="ewkino2012_ss_make_fr_from_hists --in_data plots/fake_rates/${version}/ssFR_data_standard_${version}.root \
+ --in_mc "plots/fake_rates/${version}/ssFR_wjets_standard_${version}.root,plots/fake_rates/${version}/ssFR_dy_standard_${version}.root" \
+ --output data/fake_rates/ssFR_data_ewkcor_${version}.root \
+ --ele_lumi 0.049 --mu_lumi 0.143"
+echo $cmd
+eval $cmd
