af8dfaef6446bc517408d13fd134de457501fe2f
kelley <>
2013-07-06 23:35:56 +0000

    fixed option

--- tools/build_all_bjam.sh
+++ tools/build_all_bjam.sh
@@ -6,13 +6,13 @@ function run_bjam
     local dir=$1
     local option=${2:-}
     pushd $dir
-    bjam variant=release address-model=64 architecture=ia64 threading=multi -j20 $option
-    if [ $? -eq 0 ]; then
-        echo "release build succeeded"
-    else
-        echo "release build failed"
-		exit 1
-    fi
+    #bjam variant=release address-model=64 architecture=ia64 threading=multi -j20 $option
+    #if [ $? -eq 0 ]; then
+    #    echo "release build succeeded"
+    #else
+    #    echo "release build failed"
+	#	exit 1
+    #fi
     bjam variant=debug address-model=64 architecture=ia64 threading=multi -j20 $option
     if [ $? -eq 0 ]; then
         echo "debug build succeeded"
@@ -23,17 +23,18 @@ function run_bjam
     popd
 }
 
-run_bjam $PAC/packages/cms2 
-run_bjam $PAC/externals/build/cms2_core  
-run_bjam $PAC/externals/build/cms2_frb   
-run_bjam $PAC/externals/build/MiniFWLite 
-run_bjam $PAC/externals/build/SimpleTable
-run_bjam $PAC/packages/RootTools
-run_bjam $PAC/packages/AnalysisTools
-run_bjam $PAC/packages/FGTools           
-run_bjam $PAC/analysis/ss2012
-run_bjam $PAC/analysis/ewkino2012
-run_bjam $PAC/analysis/FakeRateStudy2011 
+option=$1
+run_bjam $PAC/packages/cms2                $option
+run_bjam $PAC/externals/build/cms2_core    $option
+run_bjam $PAC/externals/build/cms2_frb     $option
+run_bjam $PAC/externals/build/MiniFWLite   $option
+run_bjam $PAC/externals/build/SimpleTable  $option
+run_bjam $PAC/packages/RootTools           $option
+run_bjam $PAC/packages/AnalysisTools       $option
+run_bjam $PAC/packages/FGTools             $option
+run_bjam $PAC/analysis/ss2012              $option
+run_bjam $PAC/analysis/ewkino2012          $option
+run_bjam $PAC/analysis/FakeRateStudy2011   $option
 
 echo ""
 echo -e ########################################
