89cadcbe9ad6f16fb7f821def9d0c42025ab92aa
kelley <>
2013-07-08 21:53:06 +0000

    new CMS2.h and added add_cms2_branches_with_tag

--- tools/build_all_bjam.sh
+++ tools/build_all_bjam.sh
@@ -6,13 +6,13 @@ function run_bjam
     local dir=$1
     local option=${2:-}
     pushd $dir
-    #bjam variant=release address-model=64 architecture=ia64 threading=multi -j20 $option
-    #if [ $? -eq 0 ]; then
-    #    echo "release build succeeded"
-    #else
-    #    echo "release build failed"
-	#	exit 1
-    #fi
+    bjam variant=release address-model=64 architecture=ia64 threading=multi -j20 $option
+    if [ $? -eq 0 ]; then
+        echo "release build succeeded"
+    else
+        echo "release build failed"
+		exit 1
+    fi
     bjam variant=debug address-model=64 architecture=ia64 threading=multi -j20 $option
     if [ $? -eq 0 ]; then
         echo "debug build succeeded"
