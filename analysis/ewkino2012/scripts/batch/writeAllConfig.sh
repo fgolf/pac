bb5133f0f701e3dd80681b05c7469740a7126266
fgolf <>
2013-09-06 21:10:13 +0000

    switch wjets to v2

--- analysis/ewkino2012/scripts/batch/writeAllConfig.sh
+++ analysis/ewkino2012/scripts/batch/writeAllConfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BABYTAG=v5 # 02-01-2013
+BABYTAG=v10 # 02-01-2013
 
 #~/~/~/~~/~/~/~~/~/~/~~/~/~/~~/~/
 # DATA TAG V05-03-24 (slimCMS2) # 
@@ -12,6 +12,7 @@ BABYTAG=v5 # 02-01-2013
 RUNLIST="\"\"\"\""
 SAMPLE=data
 ATYPE=ss
+#OPTIONS="--jet_pt_cut#20"
 OPTIONS="\"\"\"\""
 #OPTIONS="--apply_jec_unc#START52_V9#--switchSigns#1" 
 NTUPLE_PATH=/hadoop/cms/store/group/snt/papers2012/Data2012/CMSSW_5_3_2_patch4_V05-03-24
@@ -43,7 +44,6 @@ NTUPLE_PATH=/hadoop/cms/store/group/snt/papers2012/Data2012/CMSSW_5_3_2_patch4_V
 mkdir -p $ATYPE
 mv *.cmd $ATYPE/.
 
-## low pT
 # ---------------------------------------------------------#
 
 #~/~/~/~~/~/~/~~/~/~/~~/~/~/~~/~/
@@ -63,13 +63,12 @@ RUNLIST="\"\"\"\""
 ATYPE=ss
 NTUPLE_PATH=/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC
 OUTPUT_PATH=babies/ewkino2012/ss/$BABYTAG/mc
-OPTIONS="\"\"\"\""
+#OPTIONS="\"\"\"\""
 
 TAG="V05-03-23"
 CAMPAIGN=Summer12_DR53X-PU_S10_START53_V7A
 ./writeConfig.sh ttww     $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/TTWWJets_8TeV-madgraph_${CAMPAIGN}-v1/$TAG/                                  $OUTPUT_PATH/TTWWJets_8TeV-madgraph_${CAMPAIGN}-v1
 ./writeConfig.sh ttz      $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/TTZJets_8TeV-madgraph_v2_${CAMPAIGN}-v1/$TAG/                                $OUTPUT_PATH/TTZJets_8TeV-madgraph_v2_${CAMPAIGN}-v1
-./writeConfig.sh ttg      $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/TTGJets_8TeV-madgraph_${CAMPAIGN}-v1/$TAG/                                   $OUTPUT_PATH/TTGJets_8TeV-madgraph_${CAMPAIGN}-v1
 ./writeConfig.sh wz       $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WZJetsTo3LNu_TuneZ2_8TeV-madgraph-tauola_${CAMPAIGN}-v1/$TAG                 $OUTPUT_PATH/WZJetsTo3LNu_TuneZ2_8TeV-madgraph-tauola_${CAMPAIGN}-v1
 ./writeConfig.sh zz       $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/ZZJetsTo4L_TuneZ2star_8TeV-madgraph-tauola_${CAMPAIGN}-v1/$TAG/              $OUTPUT_PATH/ZZJetsTo4L_TuneZ2star_8TeV-madgraph-tauola_${CAMPAIGN}-v1
 ./writeConfig.sh ww       $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WWJetsTo2L2Nu_TuneZ2star_8TeV-madgraph-tauola_${CAMPAIGN}-v1/$TAG/           $OUTPUT_PATH/WWJetsTo2L2Nu_TuneZ2star_8TeV-madgraph-tauola_${CAMPAIGN}-v1
@@ -96,8 +95,6 @@ CAMPAIGN=Summer12_DR53X-PU_S10_START53_V7A
 ./writeConfig.sh wzz $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WZZNoGstarJets_8TeV-madgraph_${CAMPAIGN}-v1/$TAG/ $OUTPUT_PATH/WZZNoGstarJets_8TeV-madgraph_${CAMPAIGN}-v1
 ./writeConfig.sh wwz $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WWZNoGstarJets_8TeV-madgraph_${CAMPAIGN}-v1/$TAG/ $OUTPUT_PATH/WWZNoGstarJets_8TeV-madgraph_${CAMPAIGN}-v1
 ./writeConfig.sh zzz $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/ZZZNoGstarJets_8TeV-madgraph_${CAMPAIGN}-v1/$TAG/ $OUTPUT_PATH/ZZZNoGstarJets_8TeV-madgraph_${CAMPAIGN}-v1
-./writeConfig.sh ttdil $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/TTJets_FullLeptMGDecays_8TeV-madgraph_${CAMPAIGN}-v2/$TAG/     $OUTPUT_PATH/TTJets_FullLeptMGDecays_8TeV-madgraph_${CAMPAIGN}-v2
-./writeConfig.sh ttslq $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/TTJets_SemiLeptMGDecays_8TeV-madgraph_${CAMPAIGN}_ext-v1/$TAG/ $OUTPUT_PATH/TTJets_SemiLeptMGDecays_8TeV-madgraph_${CAMPAIGN}_ext-v1
 ./writeConfig.sh ttotr $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/TTJets_HadronicMGDecays_8TeV-madgraph_${CAMPAIGN}_ext-v1/$TAG                  $OUTPUT_PATH/TTJets_HadronicMGDecays_8TeV-madgraph_${CAMPAIGN}_ext-v1
 
 TAG="V05-03-25"
@@ -111,20 +108,25 @@ CAMPAIGN=Summer12_DR53X-PU_S10_START53_V7A
 ./writeConfig.sh wjets   $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/W3JetsToLNu_TuneZ2Star_8TeV-madgraph_${CAMPAIGN}-v1/$TAG   $OUTPUT_PATH/W3JetsToLNu_TuneZ2Star_8TeV-madgraph_${CAMPAIGN}-v1
 ./writeConfig.sh wjets   $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/W4JetsToLNu_TuneZ2Star_8TeV-madgraph_${CAMPAIGN}-v1/$TAG   $OUTPUT_PATH/W4JetsToLNu_TuneZ2Star_8TeV-madgraph_${CAMPAIGN}-v1
 
+CAMPAIGN=Summer12_DR53X-PU_S10_START53_V7C
+./writeConfig.sh ttdil $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/TTJets_FullLeptMGDecays_8TeV-madgraph-tauola_${CAMPAIGN}-v2/$TAG/ $OUTPUT_PATH/TTJets_FullLeptMGDecays_8TeV-madgraph-tauola_${CAMPAIGN}-v2
+./writeConfig.sh ttslq $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/TTJets_SemiLeptMGDecays_8TeV-madgraph-tauola_${CAMPAIGN}-v1/$TAG/ $OUTPUT_PATH/TTJets_SemiLeptMGDecays_8TeV-madgraph-tauola_${CAMPAIGN}-v1
+
 TAG="V05-03-27"
 CAMPAIGN=Summer12_DR53X-PU_S10_START53_V7A
 ./writeConfig.sh wgstar2m $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WGstarToLNu2Mu_TuneZ2star_7TeV-madgraph-tauola_${CAMPAIGN}-v1/$TAG/          $OUTPUT_PATH/WGstarToLNu2Mu_TuneZ2star_7TeV-madgraph-tauola_${CAMPAIGN}-v1
 ./writeConfig.sh wgstar2t $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WGstarToLNu2Tau_TuneZ2star_7TeV-madgraph-tauola_${CAMPAIGN}-v1/$TAG/         $OUTPUT_PATH/WGstarToLNu2Tau_TuneZ2star_7TeV-madgraph-tauola_${CAMPAIGN}-v1
-
-TAG="V05-03-13_slim"
-CAMPAIGN=Summer12_DR53X-PU_S10_START53_V7A
-./writeConfig.sh wjets $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball_${CAMPAIGN}-v1/$TAG/ $OUTPUT_PATH/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball_${CAMPAIGN}-v1
 
 TAG="V05-03-28"
 CAMPAIGN=Summer12_DR53X-PU_S10_START53_V7A
 ./writeConfig.sh wh_zh_tth_hww $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WH_ZH_TTH_HToWW_M-125_8TeV-pythia6_${CAMPAIGN}-v1/$TAG/                      $OUTPUT_PATH/WH_ZH_TTH_HToWW_M-125_8TeV-pythia6_${CAMPAIGN}-v1
 ./writeConfig.sh wh_zh_tth_hzz $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WH_ZH_TTH_HToZZ_M-125_8TeV-pythia6_${CAMPAIGN}-v1/$TAG/                      $OUTPUT_PATH/WH_ZH_TTH_HToZZ_M-125_8TeV-pythia6_${CAMPAIGN}-v1
 ./writeConfig.sh wh_zh_tth_htt $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WH_ZH_TTH_HToTauTau_M-125_lepdecay_8TeV-pythia6-tauola_${CAMPAIGN}-v1/$TAG/  $OUTPUT_PATH/WH_ZH_TTH_HToTauTau_M-125_lepdecay_8TeV-pythia6-tauola_${CAMPAIGN}-v1
+./writeConfig.sh wjets $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball_${CAMPAIGN}-v2/$TAG/ $OUTPUT_PATH/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball_${CAMPAIGN}-v2
+
+TAG="V05-03-30"
+CAMPAIGN=Summer12_DR53X-PU_S10_START53_V19
+./writeConfig.sh ttg      $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/TTGJets_8TeV-madgraph_${CAMPAIGN}-v1/$TAG/                $OUTPUT_PATH/TTGJets_8TeV-madgraph_${CAMPAIGN}-v1
 
 ## QCD
 #TAG=V05-03-18_slim
@@ -143,6 +145,13 @@ CAMPAIGN=Summer12_DR53X-PU_S10_START53_V7A
 #./writeConfig.sh qcd $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/QCD_Pt-80to120_TuneZ2star_8TeV_pythia6_${CAMPAIGN}-v3/$TAG/                $OUTPUT_PATH/QCD_Pt-80to120_TuneZ2star_8TeV_pythia6_${CAMPAIGN}-v3
 #./writeConfig.sh qcd $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/QCD_Pt-120to170_TuneZ2star_8TeV_pythia6_${CAMPAIGN}-v3/$TAG/               $OUTPUT_PATH/QCD_Pt-120to170_TuneZ2star_8TeV_pythia6_${CAMPAIGN}-v3
 #./writeConfig.sh qcd $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH/QCD_Pt-170to300_TuneZ2star_8TeV_pythia6_${CAMPAIGN}-v2/$TAG/               $OUTPUT_PATH/QCD_Pt-170to300_TuneZ2star_8TeV_pythia6_${CAMPAIGN}-v2
+
+TAG="V05-03-31"
+OPTIONS="--sparms#1#--isFastSim#1"
+NTUPLE_PATH=/hadoop/cms/store/user/fgolf/CMS2_V05-03-31/WinoNLSP_WinoNLSP/postprocessed/
+OUTPUT_PATH=babies/ewkino2012/ss/$BABYTAG/mc
+
+./writeConfig.sh tchiwh $ATYPE $RUNLIST $OPTIONS $NTUPLE_PATH $OUTPUT_PATH/WinoNLSP_WinoNLSP
 
 mkdir -p mc
 mv *.cmd mc/.
