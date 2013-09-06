e6ed9ea39e2ac1a1a677d704f98e12d0855f769e
fgolf <>
2013-09-06 20:15:56 +0000

    use 130 mass point as default

--- analysis/ewkino2012/scripts/do_all_hybrid_yields.sh
+++ analysis/ewkino2012/scripts/do_all_hybrid_yields.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-out_name=sswh_v9_hybrid
+out_name=sswh_v10
 lumi=19.5
 sr=-1
 nev=-1
@@ -9,10 +9,10 @@ options=""
 
 #./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --hybrid --print_evt_list --categorize_rare --signal tchiwh-150-1,tchiwh-200-1,tchiwh-250-1,tchiwh-300-1 --print_evt_list >& logs/yields_${out_name}_incl_ss.log
 
-./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --hybrid --print_evt_list --categorize_rare --signal tchiwh-150-1 --print_evt_list --suffix all >& logs/yields_${out_name}_incl_ss.log &
+./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --hybrid --print_evt_list --categorize_rare --signal tchiwh-130-1 --print_evt_list --suffix all >& logs/yields_${out_name}_incl_ss.log &
 
 options="--norm_hist"
 
 #./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --hybrid --print_evt_list --categorize_rare --signal tchiwh-150-1,tchiwh-200-1,tchiwh-250-1,tchiwh-300-1 --print_evt_list >& logs/yields_${out_name}_incl_ss.log
 
-./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --hybrid --print_evt_list --categorize_rare --signal tchiwh-150-1 --print_evt_list --suffix all >& logs/yields_${out_name}_incl_ss.log &
+./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --hybrid --print_evt_list --categorize_rare --signal tchiwh-130-1 --print_evt_list --suffix all >& logs/yields_${out_name}_incl_ss.log &
--- analysis/ewkino2012/scripts/do_all_mc_yields.sh
+++ analysis/ewkino2012/scripts/do_all_mc_yields.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-out_name=sswh_v9
+out_name=sswh_v10
 lumi=19.5
 sr=-1
 nev=-1
 options=""
 
-./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --mc_only --print_evt_list --combine_bkgds --signal tchiwh-150-1 --print_evt_list --suffix all >& logs/yields_${out_name}_incl_ss.log &
+./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --mc_only --print_evt_list --combine_bkgds --signal tchiwh-130-1 --print_evt_list --suffix all >& logs/yields_${out_name}_incl_ss.log &
 
 options="--norm_hist"
 
-./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --mc_only --print_evt_list --combine_bkgds --signal tchiwh-150-1 --print_evt_list --suffix all >& logs/yields_${out_name}_incl_ss.log &
+./scripts/ewkino2012_plot_all.py --sr $sr --anal_type ss --out_name $out_name --lumi $lumi ${options} --mc_only --print_evt_list --combine_bkgds --signal tchiwh-130-1 --print_evt_list --suffix all >& logs/yields_${out_name}_incl_ss.log &
