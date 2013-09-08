8da2d17bd59205cf4ecb99bc4ab5a21311687e43
kelleyrw <kelleyrw@gmail.com>
2013-09-08 14:59:09 -0700

    added missing cms2_tools/checkout.sh

---
+++ externals/build/cms2_tools/checkout.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# set the tag and destination folder
+tag=${1:-master}
+
+# checkout Tools
+# ------------------------------------------------------------------------------------------ #
+
+path="../../source/cms2_tools"
+dest=${path}
+echo checking out CMS2/NtupleMacros/Tools with tag $tag
+echo destination: $dest
+
+mkdir -p $dest
+pushd $dest
+
+# pull from git
+echo "git init Tools"
+git init Tools
+pushd Tools
+
+echo "git remote add origin https://github.com/cmstas/Tools.git"
+git remote add origin https://github.com/cmstas/Tools.git
+
+echo "git pull https://github.com/cmstas/Tools.git master"
+git pull https://github.com/cmstas/Tools.git master
+
+echo "git pull https://github.com/cmstas/Tools.git tag $tag"
+git pull https://github.com/cmstas/Tools.git tag $tag
+
+popd # Tools
+popd # $dest
