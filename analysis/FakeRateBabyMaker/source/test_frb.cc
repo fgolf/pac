265a6a012e46744af6ee2995f983cd90953286ce
Ryan <rwkelley@physics.ucsd.edu>
2013-11-16 14:20:36 -0800

    first commit of frb_ntuple_maker

---
+++ analysis/FakeRateBabyMaker/source/test_frb.cc
@@ -0,0 +1,22 @@
+#include "myBabyMaker.h"
+#include "TChain.h"
+
+int main()
+{
+        // create chain
+    TChain chain("Events");
+    chain.Add("/nfs-7/userdata/rwkelley/cms2/ttjets_powheg_cms2.root");
+    //chain.Add("/hadoop/cms/store/group/snt/papers2012/Data2012/CMSSW_5_3_2_patch4_V05-03-18/DoubleMu_Run2012A-13Jul2012-v1_AOD/merged/merged_ntuple_999999_1.root");
+    //chain.Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/TT_CT10_TuneZ2star_8TeV-powheg-tauola_Summer12_DR53X-PU_S10_START53_V7A-v2/V05-03-13_slim/merged_ntuple_100.root");
+    //chain.Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/QCD_Pt_20_MuEnrichedPt_15_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v3/V05-03-18_slim/merged_ntuple_100.root");
+
+    // run babymaker
+    myBabyMaker babymaker;
+    babymaker.SetNumEvents(-1);
+    babymaker.SetVerbose(false);
+    //babymaker.SetGoodRunList("json/final_19p49fb_cms2.txt");
+    babymaker.SetGoodRunList("");
+    //babymaker.ScanChain(&chain, "output/test_frb.root", /*eormu = */-1, /*apply fo filter =*/true, /*jet corr path = */"BatchSubmitFRB/job_input/data");
+    ///babymaker.ScanChain(&chain, "output/test_frb.root", /*eormu = */-1, /*apply fo filter =*/true, /*jet corr path = */"../../externals/source/cms2_core/HEAD/CORE/jetcorr/data");
+    babymaker.ScanChain(&chain, "output/test_frb.root", /*eormu = */-1, /*apply fo filter =*/true, /*jet corr path = */"/home/users/rwkelley/temp/pac/externals/source/cms2_core/ss2012_V02-05-12/CORE/jetcorr/data");
+}
