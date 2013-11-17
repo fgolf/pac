2f98513fad5568d09cf97ca0b5a9c0b4a3fea692
Ryan <rwkelley@physics.ucsd.edu>
2013-11-16 23:10:53 -0800

    updated processData.C

--- analysis/FakeRateBabyMaker/batch/job_input/processData.C
+++ analysis/FakeRateBabyMaker/batch/job_input/processData.C
@@ -1,12 +1,5 @@
 void processData(std::string output_file, int datatype, std::string input_file, int num_events, std::string run_list)
 {
-    // add local path to LD_LIBRARY_PATH 
-    gSystem->AddDynamicPath(gSystem->Getenv("PWD"));
-    gSystem->Exec("LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD");
-    gSystem->Exec("PATH=$PATH:$PWD");
-/*     cout << "gSystem->Getenv("LD_LIBRARY_PATH") << endl; */
-/*     cout << "Dynamic path = " << gSystem->GetDynamicPath() << endl; */
-
     // construct command string
     std::string cmd = "./create_fake_rate_baby";
     cmd += Form(" --output %s.root" , output_file.c_str()); 
