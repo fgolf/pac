f1872f14098ea844b8eae236fd20e85e2026a5ca
kelleyrw <kelleyrw@gmail.com>
2013-11-16 20:01:30 -0800

    fixed very subtle bug with compiling myBabyMaker

--- analysis/FakeRateBabyMaker/source/create_fake_rate_baby.cc
+++ analysis/FakeRateBabyMaker/source/create_fake_rate_baby.cc
@@ -1,22 +1,133 @@
 #include "myBabyMaker.h"
+#include "rt/RootTools.h"
 #include "TChain.h"
+#include <string>
+#include <iostream>
+#include <boost/program_options.hpp>
 
-int main()
+int main(int argc, char* argv[])
+ try
 {
-        // create chain
+    // set the style
+    rt::SetStyle("emruoi");
+
+    using namespace std;
+
+    // inputs
+    // -------------------------------------------------------------------------------------------------//
+
+    std::string input_file          = "";
+    std::string output_file         = "fake_rate_baby_tree.root";
+    std::string jetcorr_path        = rt::getenv("PAC") + "/externals/source/cms2_core/CORE/jetcorr/data";
+    std::string good_run_list       = "";
+    long number_of_events           = -1;
+    short e_or_mu                   = -1;
+    bool apply_filter               = false;
+    bool verbose                    = false;
+
+    namespace po = boost::program_options;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help"     , "print this menu")
+        ("input"    , po::value<std::string>(&input_file)->required() , "name of input ROOT file"                                  )
+        ("output"   , po::value<std::string>(&output_file)            , "name of output ROOT file"                                 )
+        ("run_list" , po::value<std::string>(&good_run_list)          , "good Run list (no default)"                               )
+        ("jetcorr"  , po::value<std::string>(&good_run_list)          , "path to jet correction files (default is \"data\")"       )
+        ("nev"      , po::value<long>(&number_of_events)              , "number of events to run on (-1 == all)"                   )
+        ("eormu"    , po::value<short>(&e_or_mu)                      , "electron or muon (11 == electron, 13 == muon, -1 == both)")
+        ("filter"   , po::value<bool>(&apply_filter)                  , "apply filter (default == false)"                          )
+        ("verbose"  , po::value<bool>(&verbose)                       , "verbosity (default == false)"                             )
+        ;
+
+    // parse it
+    try
+    {
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+
+        if (vm.count("help")) 
+        {
+            cout << desc << "\n";
+            return 1;
+        }
+
+        po::notify(vm);
+    }
+    catch (const std::exception& e)
+    {
+        cerr << e.what() << "\nexiting" << endl;
+        cout << desc << "\n";
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown error!" << "\n";
+        return false;
+    }
+
+    // check that input file exists and is specified
+    if (!input_file.empty())
+    { 
+        if (rt::ls(input_file).empty())
+        {
+            cout << "[create_fake_rate_baby] Error : input file " << input_file << " not found" << endl;
+            cout << desc << "\n";
+            return 1;
+        }
+    }
+
+    // check e_or_mu is valid 
+    if (not (e_or_mu == 11 or e_or_mu == 13 or e_or_mu == -1))
+    {
+        cout << "[create_fake_rate_baby] Error : eormu value is invalid." << endl;
+        cout << desc << "\n";
+        return 1;
+    }
+
+    // check that the jetcorr path is a valid directory
+    if (not rt::exists(jetcorr_path))
+    {
+        cout << "[create_fake_rate_baby] Error : input file " << jetcorr_path << " not found" << endl;
+        return 1;
+    }
+
+
+    // inputs
+    cout << "inputs:" << endl;
+    cout << "input_file       :\t" << input_file       << endl;
+    cout << "output_file      :\t" << output_file      << endl;
+    cout << "jetcorr_path     :\t" << jetcorr_path     << endl;
+    cout << "good_run_list    :\t" << good_run_list    << endl;
+    cout << "number_of_events :\t" << number_of_events << endl;
+    cout << "e_or_mu          :\t" << e_or_mu          << endl;
+    cout << "apply_filter     :\t" << apply_filter     << endl;
+    cout << "verbose          :\t" << verbose          << endl;
+
+    // do the main analysis
+    // -------------------------------------------------------------------------------------------------//
+
+    // create the chain
     TChain chain("Events");
-    chain.Add("/nfs-7/userdata/rwkelley/cms2/ttjets_powheg_cms2.root");
-    //chain.Add("/hadoop/cms/store/group/snt/papers2012/Data2012/CMSSW_5_3_2_patch4_V05-03-18/DoubleMu_Run2012A-13Jul2012-v1_AOD/merged/merged_ntuple_999999_1.root");
-    //chain.Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/TT_CT10_TuneZ2star_8TeV-powheg-tauola_Summer12_DR53X-PU_S10_START53_V7A-v2/V05-03-13_slim/merged_ntuple_100.root");
-    //chain.Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/QCD_Pt_20_MuEnrichedPt_15_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v3/V05-03-18_slim/merged_ntuple_100.root");
+    rt::MakeTChain(input_file, chain.GetName(), &chain, verbose);
+    rt::PrintFilesFromTChain(chain);
 
     // run babymaker
     myBabyMaker babymaker;
-    babymaker.SetNumEvents(1);
-    babymaker.SetVerbose(false);
-    //babymaker.SetGoodRunList("json/final_19p49fb_cms2.txt");
-    babymaker.SetGoodRunList("");
-    //babymaker.ScanChain(&chain, "output/test_frb.root", /*eormu = */-1, /*apply fo filter =*/true, /*jet corr path = */"BatchSubmitFRB/job_input/data");
-    ///babymaker.ScanChain(&chain, "output/test_frb.root", /*eormu = */-1, /*apply fo filter =*/true, /*jet corr path = */"../../externals/source/cms2_core/HEAD/CORE/jetcorr/data");
-    babymaker.ScanChain(&chain, "test_frb.root", /*eormu = */-1, /*apply fo filter =*/true, /*jet corr path = */"../../externals/source/cms2_core/CORE/jetcorr/data");
+    babymaker.SetNumEvents(number_of_events);
+    babymaker.SetVerbose(verbose);
+    if (!good_run_list.empty())
+    {
+        babymaker.SetGoodRunList(good_run_list.c_str());
+    }
+    cout << "running fake rate baby maker..." << endl;
+    babymaker.ScanChain(&chain, output_file.c_str(), e_or_mu, apply_filter, jetcorr_path.c_str());
+    cout << "finished fake rate baby maker..." << endl;
+
+    // done 
+    return 0;
+}
+catch (std::exception& e)
+{
+    std::cerr << e.what() << std::endl;
+    return 1;
 }
